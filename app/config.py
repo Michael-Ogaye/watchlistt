27ab00506f69004803aac95da4990c84ff05720b
Michael Ogaye <hogaeymic@gmail.com>
2022-04-28 00:16:33 +0300

    This ia  a movie watchlist upload

---
+++ app/config.py
@@ -0,0 +1,29 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_KEY='09e41b835383809c9a7358f1e7dd753b'
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+
+
+
+
+class ProdConfig(Config):
+    '''
+    Pruduction  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+    DEBUG = True
